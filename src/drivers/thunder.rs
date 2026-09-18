260dd4a2b2251bb0fab5cc8b2d2dc1b482a3452c
evecus <evecus@163.com>
2026-09-18 13:43:49 +0800

    change

--- src/drivers/thunder.rs
+++ src/drivers/thunder.rs
@@ -1140,7 +1140,7 @@ fn sha256(data: &[u8]) -> [u8; 32] {
         msg.push(0);
     }
     msg.extend_from_slice(&bit_len.to_be_bytes());
-    for chunk in msg.chunks_exact(64) {
+    for chunk in msg.as_chunks::<64>().0 {
         let mut w = [0u32; 64];
         for (i, wi) in w.iter_mut().enumerate().take(16) {
             *wi = u32::from_be_bytes([chunk[4 * i], chunk[4 * i + 1], chunk[4 * i + 2], chunk[4 * i + 3]]);
